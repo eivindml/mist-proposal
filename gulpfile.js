0129046fb006a9ab6bd28f06dab09800b3a3f394
Eivind Mikael Lindbråten <eivind.lindbraten@gmail.com>
2017-09-08 18:24:27 +0200

    Add gulp LaTeX compile task, refactor and restructure

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	latex = require('gulp-pdflatex'),
+	del = require('del');
+
+// Watch source files
+gulp.task('watch', ['latex'], function() {
+	gulp.watch(['**/*.tex'], ['latex']);
+});
+
+// Compile LaTeX file with PDFLaTeX
+gulp.task('latex', function() {
+	return gulp.src('example.tex')
+		.pipe(latex({
+			shellEscape: true,
+			texInputs: ['.', './assets/']
+		}))
+		.pipe(gulp.dest('./build'))
+})
+
+// Clean build files
+gulp.task('clean', function() {
+	return del(['*.aux', '*.log', '*.dvi', '*.out', '*.pdf']);
+});
+
+// Set default task
+gulp.task('default', ['latex']);
